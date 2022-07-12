d59b8dab0774cd4f2a79b1bb5352ecf7f10cfc80
Jared Collier <jared@Jareds-MBP.localdomain>
2022-07-12 14:34:38 -0600

    gets + and - buttons working

--- main.js
+++ main.js
@@ -1,60 +1,17 @@
-const minusButton= document.getElementById('minus-btn');
-const plusButton = document.getElementById('plus-btn');
+// const minusButton= document.getElementById('minus-btn');
+// const plusButton = document.getElementById('plus-btn');
 
-
-function increment() {
-    document.getElementById('field').stepUp();
- }
- function decrement() {
-    document.getElementById('field').stepDown();
- };
-
- function increment() {
-   document.getElementById('field1').stepUp();
-}
-function decrement() {
-   document.getElementById('field1').stepDown();
-};
-
-function increment() {
-   document.getElementById('field2').stepUp();
-}
-function decrement() {
-   document.getElementById('field2').stepDown();
-};
-
-function increment() {
-   document.getElementById('field3').stepUp();
-}
-function decrement() {
-   document.getElementById('field3').stepDown();
+function increment(event) {
+   const clickedBtn = event.target
+   const inputField = clickedBtn.previousElementSibling
+   inputField.stepUp();
 }
 
-function increment() {
-   document.getElementById('field4').stepUp();
-}
-function decrement() {
-   document.getElementById('field4').stepDown();
-}
-
-function increment() {
-   document.getElementById('field5').stepUp();
-}
-function decrement() {
-   document.getElementById('field5').stepDown();
+function decrement(event) {
+   const clickedBtn = event.target
+   const inputField = clickedBtn.nextElementSibling
+   inputField.stepDown();
 }
 
-function increment() {
-   document.getElementById('field6').stepUp();
-}
-function decrement() {
-   document.getElementById('field6').stepDown();
-}
-function increment() {
-   document.getElementById('field7').stepUp();
-}
-function decrement() {
-   document.getElementById('field7').stepDown();
-}
- minusButton.addEventListener('click',decrement)
- plusButton.addEventListener('click',increment)
+//  minusButton.addEventListener('click',decrement)
+//  plusButton.addEventListener('click',increment)
